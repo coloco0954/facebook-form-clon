df88d15adff9e18cabe55143c76fd2dec951872a
coloco0954 <nat.lopez.jimenez@gmail.com>
2024-04-06 23:17:58 -0600

    correccion script mostrar password

--- src/js/show-password.js
+++ src/js/show-password.js
@@ -4,17 +4,17 @@ const eyeImage = document.querySelector('#eye');
 passwordInput?.addEventListener('keyup', function(){
     if(passwordInput.value.length >= 1){
         eyeImage?.classList.remove('opacity-0');
-
-        eyeImage?.addEventListener('click', function(){
-            if(passwordInput.type == 'password'){
-                passwordInput.type = 'text';
-                eyeImage.src = '/eye.svg';
-            } else {
-                passwordInput.type = 'password';
-                eyeImage.src = '/eye-slash.svg';
-            }
-        })
     } else {
         eyeImage?.classList.add('opacity-0')
+    }
+})
+
+eyeImage?.addEventListener('click', function(){
+    if(passwordInput.type == 'password'){
+        passwordInput.type = 'text';
+        eyeImage.src = '/eye.svg';
+    } else {
+        passwordInput.type = 'password';
+        eyeImage.src = '/eye-slash.svg';
     }
 })
